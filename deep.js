05c0db0bfc5cab186d981968b440b47dd364304e
NwanmaRichard <70892172+NwanmaRichard@users.noreply.github.com>
2022-04-26 22:10:08 +0100

    Update deep.js

--- deep.js
+++ deep.js
@@ -3,4 +3,4 @@ const plain = (arr) => {
 };
 let array = [1, 2, [3, 4, [5]]];
 console.log(plain(array));
-I created a function named plain that takes in an array and i used the flat() method on the array
+//I created a function named plain that takes in an array and i used the flat() method on the array
