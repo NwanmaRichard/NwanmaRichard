850f662a15f9599cc62eeaad4090cc1f5829e54f
NwanmaRichard <70892172+NwanmaRichard@users.noreply.github.com>
2022-05-08 22:01:31 +0100

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const person = {
+  name: "Daniel",
+  eyeColor: "Blue",
+  age: 27,
+};
+
+//////OR//////
+
+const person2 = new Object();
+person2.age = 40;
+person2.name = "Daniel";
